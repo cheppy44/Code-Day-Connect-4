4a44de232fb3d52eab078f5989918a82f2f7eca9
Owen Hoffend <mcnewomaster@gmail.com>
2015-11-07 15:37:40 -0600

    Now I fixed it.

--- com/game/unitTests/GridTest.java
+++ com/game/unitTests/GridTest.java
@@ -1,23 +1,24 @@
 package com.game.unitTests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 import com.game.connect4.Grid;
+import com.game.connect4.State;
 
 public class GridTest {
 
 	@Test
 	public void test() {
-		Grid grid = new Grid(7,6);
+		Grid grid = new Grid(7, 6);
 		grid.newGame();
-		int yCoordinate1 = grid.dropGamePiece(1, Grid.state.red);
-		int yCoordinate2 = grid.dropGamePiece(1, Grid.state.yellow);
-		
-		assertEquals(1,yCoordinate1);
-		assertEquals(2,yCoordinate2);
-		
+		int yCoordinate1 = grid.dropGamePiece(1, State.red);
+		int yCoordinate2 = grid.dropGamePiece(1, State.yellow);
+
+		assertEquals(1, yCoordinate1);
+		assertEquals(2, yCoordinate2);
+
 	}
 
 }
