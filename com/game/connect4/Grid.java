1b4dbb53d80e84b65d946b48b48a296b19d55711
Nathan Godfrey <ndgodfr@gmail.com>
2015-11-07 13:48:28 -0600

    Working on grid array.

--- com/game/connect4/Grid.java
+++ com/game/connect4/Grid.java
@@ -1,9 +1,24 @@
 package com.game.connect4;
 
+import java.util.Set;
+
 public class Grid {
 	
-	public Grid(int xSize, int ySize) {
-		
+	int xWidth;
+	int yHeight;
+	int area;
+	
+	private enum State{empty,red,yellow}
+	
+	private State[][] grid;
+	
+	public Grid(int xWidth, int yHeight) {
+		this.xWidth = xWidth;
+		this.yHeight = yHeight;
+		area = xWidth * yHeight;
+		this.grid = new State[xWidth][yHeight];
 	}
+	
+	
 
 }
