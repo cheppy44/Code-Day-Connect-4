b75f029f96037e2199e78e8337e9d7eec11b3e11
Nathan Godfrey <ndgodfr@gmail.com>
2015-11-07 18:16:12 -0600

    Finished Win Detection and now working on unit tests to be sure it works and doesn't move beyond array bounds.

--- com/game/connect4/WinDetection.java
+++ com/game/connect4/WinDetection.java
@@ -2,32 +2,31 @@ package com.game.connect4;
 
 public class WinDetection {
 	private Grid grid;
-	
+
 	public WinDetection(Grid grid) {
 		this.grid = grid;
 	}
-	
-	public boolean detectWin(int lastMoveX,int lastMoveY,State lastPlayer) {
-		
-		if (detect4Horizontal(lastMoveX,lastMoveY,lastPlayer) || 
-		detect4Vertical(lastMoveX,lastMoveY,lastPlayer) ||
-		detect4DiagonalUp(lastMoveX,lastMoveY,lastPlayer) ||
-		detect4DiagonalDown(lastMoveX,lastMoveY,lastPlayer)) {
+
+	public boolean detectWin(int lastMoveX, int lastMoveY, State lastPlayer) {
+
+		if (detect4Horizontal(lastMoveX, lastMoveY, lastPlayer) || detect4Vertical(lastMoveX, lastMoveY, lastPlayer)
+				|| detect4DiagonalUp(lastMoveX, lastMoveY, lastPlayer)
+				|| detect4DiagonalDown(lastMoveX, lastMoveY, lastPlayer)) {
 			return true;
 		}
-		
+
 		return false;
 	}
-	
-	private boolean detect4Horizontal(int x,int y,State state) {
+
+	private boolean detect4Horizontal(int x, int y, State state) {
 		int filledSlots = 1;
-		for (int i = x + 1;i >= 0 && i <= grid.getxWidth();i++) {
+		for (int i = x + 1; i >= 0 && i <= grid.getxWidth(); i++) {
 			if (grid.getSlot(i, y) != state) {
 				break;
 			}
 			filledSlots++;
 		}
-		for (int i = x - 1;i >= 0 && i <= grid.getxWidth();i--) {
+		for (int i = x - 1; i >= 0 && i <= grid.getxWidth(); i--) {
 			if (grid.getSlot(i, y) != state) {
 				break;
 			}
@@ -38,17 +37,17 @@ public class WinDetection {
 		}
 		return false;
 	}
-	
-	private boolean detect4Vertical(int x,int y,State state) {
+
+	private boolean detect4Vertical(int x, int y, State state) {
 		int filledSlots = 1;
-		for (int i = y + 1;i >= 0 && i <= grid.getyHeight();i++) {
-			if (grid.getSlot(x,i) != state) {
+		for (int i = y + 1; i >= 0 && i <= grid.getyHeight(); i++) {
+			if (grid.getSlot(x, i) != state) {
 				break;
 			}
 			filledSlots++;
 		}
-		for (int i = y - 1;i >= 0 && i <= grid.getyHeight();i--) {
-			if (grid.getSlot(x,i) != state) {
+		for (int i = y - 1; i >= 0 && i <= grid.getyHeight(); i--) {
+			if (grid.getSlot(x, i) != state) {
 				break;
 			}
 			filledSlots++;
@@ -58,59 +57,62 @@ public class WinDetection {
 		}
 		return false;
 	}
-	
-	private boolean detect4DiagonalUp(int x,int y,State state) {
+
+	private boolean detect4DiagonalUp(int x, int y, State state) {
 		int filledSlots = 1;
-		if (grid.getSlot(x + 1, y + 1) == state) {
-			filledSlots++;
-			if (grid.getSlot(x + 2, y + 1) == state) {
-				filledSlots++;
-				if (grid.getSlot(x + 3, y + 1) == state) {
-					filledSlots++;
-				}
+		int dI = 1;
+		for (int i = x + 1; i >= 0 && i <= grid.getxWidth(); i++) {
+			if (dI >= 0 && dI <= grid.getyHeight()) {
+				break;
 			}
+			if (grid.getSlot(i, y + dI) != state) {
+				break;
+			}
+
+			filledSlots++;
 		}
-		if (grid.getSlot(x - 1, y - 1) == state) {
-			filledSlots++;
-			if (grid.getSlot(x - 2, y - 2) == state) {
-				filledSlots++;
-				if (grid.getSlot(x - 3, y - 3) == state) {
-					filledSlots++;
-				}
+		for (int i = x - 1; i >= 0 && i <= grid.getxWidth(); i--) {
+			if (dI >= 0 && dI <= grid.getyHeight()) {
+				break;
 			}
+			if (grid.getSlot(i, y - dI) != state) {
+				break;
+			}
+			filledSlots++;
 		}
 		if (filledSlots == 4) {
 			return true;
 		}
-		
 		return false;
 	}
-	
-	private boolean detect4DiagonalDown(int x,int y,State state) {
+
+	private boolean detect4DiagonalDown(int x, int y, State state) {
 		int filledSlots = 1;
-		if (grid.getSlot(x + 1, y + 1) == state) {
-			filledSlots++;
-			if (grid.getSlot(x + 2, y + 1) == state) {
-				filledSlots++;
-				if (grid.getSlot(x + 3, y + 1) == state) {
-					filledSlots++;
-				}
+		int dI = 1;
+		for (int i = x + 1; i >= 0 && i <= grid.getxWidth(); i++) {
+			if (dI >= 0 && dI <= grid.getyHeight()) {
+				break;
 			}
+			if (grid.getSlot(i, y - dI) != state) {
+				break;
+			}
+
+			filledSlots++;
 		}
-		if (grid.getSlot(x - 1, y - 1) == state) {
-			filledSlots++;
-			if (grid.getSlot(x - 2, y - 2) == state) {
-				filledSlots++;
-				if (grid.getSlot(x - 3, y - 3) == state) {
-					filledSlots++;
-				}
+		for (int i = x - 1; i >= 0 && i <= grid.getxWidth(); i--) {
+			if (dI >= 0 && dI <= grid.getyHeight()) {
+				break;
 			}
+			if (grid.getSlot(i, y + dI) != state) {
+				break;
+			}
+			filledSlots++;
 		}
 		if (filledSlots == 4) {
 			return true;
 		}
-		
 		return false;
+
 	}
-	
+
 }
---
+++ com/game/unitTests/WinDetectionTest.java
@@ -0,0 +1,14 @@
+package com.game.unitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WinDetectionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
