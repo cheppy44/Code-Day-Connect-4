dc6ec467ce596516e6eb76c716abc98359752d5f
Owen Hoffend <mcnewomaster@gmail.com>
2015-11-07 19:20:17 -0600

    Beginning some network control methods

--- com/game/graphics/Screen.java
+++ com/game/graphics/Screen.java
@@ -12,7 +12,6 @@ public class Screen {
 
 	public void update() { //Update screen. Drawing happens in this method.
 		Graphics g = image.getGraphics();
-
 		//Image drawing commands here. Use g to draw onto the image.
 
 		g.drawImage(image, 0, 0, null);
--- com/game/network/Network.java
+++ com/game/network/Network.java
@@ -18,12 +18,27 @@ public class Network {
 		}
 	}
 
-	public void update() { //This method will first take input from the board, then propagate the signal through the network.
+	public void update() { //This method will first take input from the board, then propagate the signal through the network. This update does signal stuff, not structure stuff
+		for (int x = 0; x < grid.getxWidth(); x++) {
+			for (int y = 0; y < grid.getyHeight(); y++) {
+				nodes.get(x + grid.getxWidth() * y).setState(grid.getState()[x][y]);
+			}
+		}
+	}
+
+	public void connectNodes() {
 
 	}
 
 	public void addNewNode() {
+		int newIndex = nodes.size() + 1;
+		nodes.add(new Node(newIndex));
+	}
 
+	public void removeNode(int index) {
+		if (index > grid.getArea()) { //makes sure that a node isn't removed from the grid board game input nodes. 
+
+		}
 	}
 
 	public List<Node> getNodes() {
