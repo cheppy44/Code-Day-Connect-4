298be7e997a1bf7c02714541f2538295ee958b95
Owen Hoffend <mcnewomaster@gmail.com>
2015-11-08 03:39:25 -0600

    I'm tired

--- com/game/connect4/Connect4.java
+++ com/game/connect4/Connect4.java
@@ -2,7 +2,6 @@ package com.game.connect4;
 
 import com.game.evolution.NetworkHandler;
 import com.game.graphics.GUI;
-import com.game.network.Network;
 import com.game.network.NetworkVisualizer;
 
 public class Connect4 {
@@ -13,22 +12,13 @@ public class Connect4 {
 	NetworkVisualizer visualizer;
 
 	public Connect4() {
-		handler = new NetworkHandler();
 		gameGrid = new Grid(7, 6);
-		network = new Network(gameGrid);
+		handler = new NetworkHandler(gameGrid);
 		new Thread(gui = new GUI(gameGrid));
-		new Thread(visualizer = new NetworkVisualizer(network.getNodes()));
-		visualizer.start();
+		//new Thread(visualizer = new NetworkVisualizer(network.getNodes())); //Visualization lives here when we want to enable that again
+		//visualizer.start();
 		gui.start();
 
-	}
-
-	public synchronized void start() {
-		running = true;
-	}
-
-	public synchronized void stop() {
-		running = false;
 	}
 
 	public static void main(String[] args) {
--- com/game/evolution/NetworkHandler.java
+++ com/game/evolution/NetworkHandler.java
@@ -1,12 +1,23 @@
 package com.game.evolution;
 
+import com.game.connect4.GameRunner;
+import com.game.connect4.Grid;
 import com.game.network.Network;
 
-public class NetworkHandler {
+public class NetworkHandler implements Runnable {
 
+	private Grid grid;
+	private GameRunner runner;
 	private Network[] population;
 
-	public void mutateNetwork() {
+	public NetworkHandler(Grid grid) {
+		this.grid = grid;
+		runner = new GameRunner(grid);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
 
 	}
 }
