39162624b267f016bbc712294df2bb4a12ccc746
Kevin La Casse <wolfklacasse@gmail.com>
2015-11-07 19:23:26 -0600

    Began GUI stuff

--- com/game/connect4/Connect4.java
+++ com/game/connect4/Connect4.java
@@ -1,13 +1,38 @@
 package com.game.connect4;
 
+import com.game.graphics.GUI;
 import com.game.network.Network;
 
-public class Connect4 {
+public class Connect4 implements Runnable {
 	public Grid gameGrid;
 	public Network network;
+	private boolean running = false; 
 
 	public Connect4() {
 		gameGrid = new Grid(7, 6);
 		network = new Network(gameGrid);
 	}
+	
+	public synchronized void start(){
+		running = true;
+		new Thread(this).start();
+	}
+	
+	public synchronized void stop(){
+		running = false; 
+	}
+
+	@Override
+	public void run() {
+		while(running){
+			GUI gui = new GUI();
+			gui.renderScreen();
+		}
+	}
+
+//	public static void main(String[] args){
+//		System.out.println("Hi");
+//		new Connect4().start();
+//	}
+	
 }
